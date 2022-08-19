84602760bfdc7471e88af2c0787d2f86c477db76
Lizaveta Petrova <goldglaider@gmail.com>
2022-08-19 22:48:55 +0300

    init: init page

---
+++ src/pages/Games/SprintGame/SprintGame.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Footer from '../../../components/Footer/Footer';
+import Header from '../../../components/Header/Header';
+import { MainPageContent } from './MainPageContent';
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default SprintGame;
