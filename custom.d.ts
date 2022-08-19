ff60e77ab8bcdba9888e4b929b280593eebf809b
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2022-08-19 23:16:32 +0300

    feat: merge develop

--- custom.d.ts
+++ custom.d.ts
@@ -11,7 +11,6 @@ declare module '*.png' {
   const content: string;
   export default content;
 }
-
 declare module '*.svg' {
   const content: string;
   export default content;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-
 import { GlobalStyle } from '../src/styles/global';
-import 'typeface-rubik';
-import Registration from './pages/Registration/Registration';
-
-import 'typeface-rubik';
-
 import './App.scss';
+import 'typeface-rubik';
+
+import Registration from './pages/Registration/Registration';
 import MainPage from './pages/MainPage/MainPage';
 
 function App() {
