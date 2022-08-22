c3d659dfd199b99604c69c1267f1af3ebb6ac0a1
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-08-22 11:48:08 +0300

    feat: title change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { GlobalStyle } from '../src/styles/global';
 import './App.scss';
 import 'typeface-rubik';
@@ -13,26 +13,29 @@ import TextBook from './pages/TextBook/TextBook';
 import { GetTitle } from './utils/utils';
 
 function App() {
-  
+  const [pageTitle, setPageTitle] = useState<string>(GetTitle(global.location.pathname) ?? 'Easy English');
+  useEffect(() => {
+    setPageTitle(() => GetTitle(global.location.pathname) ?? 'Easy English');
+  }, [useLocation()]);
   return (
     <div className="App">
       <GlobalStyle />
-      
-      <Header title={GetTitle('')} />
+
+      <Header title={pageTitle} />
 
       <Routes>
-      <Route path='/' element={
-        <MainPage />
-      }/>
-      <Route path='/registration' element={
-        <Registration />
-      }/>
-      <Route path='/textbook' element={
-        <TextBook />
-      }/>
-      <Route path='/games' element={
-        <MiniGames />
-      }/>
+        <Route path='/' element={
+          <MainPage />
+        } />
+        <Route path='/registration' element={
+          <Registration />
+        } />
+        <Route path='/textbook' element={
+          <TextBook />
+        } />
+        <Route path='/games' element={
+          <MiniGames />
+        } />
       </Routes>
 
       <Footer />
