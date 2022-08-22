cdb57c022dad60431eac32ac27eac1e3b60de0b1
Lizaveta Petrova <goldglaider@gmail.com>
2022-08-22 22:59:55 +0300

    init: init sprint game page

--- src/pages/MiniGamesPage/SprintGame/SprintGame.tsx
+++ src/pages/MiniGamesPage/SprintGame/SprintGame.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 import { GameSprintWrapper } from './SprintGame.styled';
 
 const SprintGame = () => {
-  
+  return (
+      <SprintGameWrapper>
+        <ProgressBarTime/>
+        <GameContent>
+          <GameScore></GameScore>
+          <GameLifes></GameLifes>
+          <GameCard></GameCard>
+        </GameContent>
+      </SprintGameWrapper>
+    );
 };
 
 export default SprintGame;
