f2e9e18fe8e40b85670b5dd10933ec492165f751
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2022-08-24 00:28:50 +0300

    feat: add buttons, add picture, add cheekers

--- src/pages/MiniGamesPage/AudioChallengeGame/AudioChallengeGame.styled.tsx
+++ src/pages/MiniGamesPage/AudioChallengeGame/AudioChallengeGame.styled.tsx
@@ -13,64 +13,182 @@ export const AudioChallengeWrapper = styled.div`
 export const MainBlock = styled.div`
     min-width: 280px;
     min-height: 400px;
+    margin-bottom: 327px;
     width: 100%;
     height: auto;
-    background: #fff;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    @media (max-width: 1024px) {
+        margin-bottom: 0px;
+    }
+
 `;
 
 export const InnerBlock = styled.div`
     width: 80%;
     min-height: 100%;
     height: auto;
-    background: blue;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    gap: 30px;
+    @media (max-width: 1024px) {
+        flex-direction: column;
+    }
 `;
+
 
 export const ButtonBlock = styled.div`
     width: 50%;
-    height: 100%;
-    background: orange;
+    height: auto;
+    padding-left: 170px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     gap: 18px;
+    @media (max-width: 1024px) {
+        padding-left: 0px;
+    }
+
 `;
 export const PictureBlock = styled.div`
     width: 50%;
-    height: 100%;
-    background: red;
+    height: auto;
+    padding-left: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    @media (max-width: 1024px) {
+        padding-left: 0px;
+    }
+`;
+
+export const PrimaryPicture = styled.div`
+    min-width: 404px;
+    min-height: 520px;
+    background: rgba(255, 255, 255, 0.39);
+    backdrop-filter: blur(4px);
+    border-radius: 8px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
+export const SoundPictureWrapper = styled.div`
+    width: 96px;
+    height: 96px;
+    background: #fff;
+    border-radius: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+
+export const WrongAnswerPictureWrapper = styled(SoundPictureWrapper)``;
+export const RedCircle = styled(SoundPictureWrapper)`
+    width: 72px;
+    height: 72px;
+    background: #FD1E1E;
+
+    p {
+        font-size: 45px;
+        font-weight: 300;
+        color: #fff;
+    }
+`;
+
+export const Picture = styled(PrimaryPicture)`
+    justify-content: flex-start;
+    background: #fff;
+`;
+
+export const WordWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+`;
+
+export const PlayAudioInAnswerCard = styled.div`
+    width: 20%;
+    height: 100%;
+    padding-top: 27px;
+    padding-left: 35px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+
+export const Word = styled.div`
+    width: 80%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .word {
+        margin-right: 53px;
+        margin-top: 22px;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 40px;
+        color: #000000;
+    }
+
+    .translate {
+        margin-right: 53px;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 40px;
+        color: #9B9B9B;
+    }
+`;
+
 export const AnswerButton = styled.button`
-    padding-left: 53px;
-    padding-right: 190px;
-    padding-top: 9px;
-    padding-bottom: 9px;
+    width: 328px;
+    height: 58px;
+    padding-left: 52px;
     background: linear-gradient(1.8deg, #FEFEFE -43.56%, #E6E6E6 123.62%);
     box-shadow: 3px 4px 11px rgba(0, 0, 0, 0.08);
     border-radius: 15px;
+    border: 2px solid transparent;
     cursor: pointer;
     font-weight: 500;
     font-size: 20px;
     line-height: 40px;
     letter-spacing: 0.015em;
     color: #000;
+    display: flex;
+    align-items: center;
+    gap: 15px;
+
+    &.correct {
+        border: 2px solid #fff;
+        background: linear-gradient(0.16deg, #4EE57E -94.42%, #07D6A0 321.64%);
+    }
+
+    &.wrong {
+        color: #FD1E1E;
+    }
+
+    &.current {
+        color: green;
+    }
 `;
 
 export const SkipQuestion = styled(AnswerButton)`
-    padding: 9px 108px;
+    min-width: 328px;
+    padding: 4px 97px;
     background: #FFFFFF;
     border-radius: 38px;
 `;
--- src/pages/MiniGamesPage/AudioChallengeGame/AudioChallengeGame.tsx
+++ src/pages/MiniGamesPage/AudioChallengeGame/AudioChallengeGame.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import ProgressBar from '../../../components/ProgressBar/ProgressBar';
-import { AnswerButton, AudioChallengeWrapper, ButtonBlock, InnerBlock, MainBlock, PictureBlock, SkipQuestion } from './AudioChallengeGame.styled';
+import Audio from '../../../assets/AudioBlack.svg';
+import CorrectAnswer from '../../../assets/CorrectAnswer.svg';
+import TestAnswerPicture from '../../../assets/test_answer.jpg';
+import { AnswerButton, AudioChallengeWrapper, ButtonBlock, InnerBlock, MainBlock, Picture, PictureBlock, PlayAudioInAnswerCard, PrimaryPicture, RedCircle, SkipQuestion, SoundPictureWrapper, Word, WordWrapper, WrongAnswerPictureWrapper } from './AudioChallengeGame.styled';
 
 const AudioChallengeGame = () => {
   return (
-        <AudioChallengeWrapper>
-            <MainBlock>
-            <ProgressBar percent={10}/>
-            <InnerBlock>
-                <ButtonBlock>
-                <AnswerButton>Variant</AnswerButton>
-                <AnswerButton>Variant</AnswerButton>
-                <AnswerButton>Variant</AnswerButton>
-                <AnswerButton>Variant</AnswerButton>
-                <AnswerButton>Variant</AnswerButton>
-                <SkipQuestion>I don`t know</SkipQuestion>
-                </ButtonBlock>
-                <PictureBlock></PictureBlock>
-            </InnerBlock>
-            </MainBlock>
-        </AudioChallengeWrapper>
+    <AudioChallengeWrapper>
+      <MainBlock>
+        <ProgressBar percent={10} />
+        <InnerBlock>
+          <ButtonBlock>
+            <AnswerButton className='current'>Variant 1</AnswerButton>
+            <AnswerButton className='correct'>Variant</AnswerButton>
+            <AnswerButton className='wrong'>Variant</AnswerButton>
+            <AnswerButton>Variant</AnswerButton>
+            <AnswerButton>Variant</AnswerButton>
+            <SkipQuestion>Don`t know</SkipQuestion>
+          </ButtonBlock>
+          <PictureBlock>
+{/*             <PrimaryPicture>
+              <WrongAnswerPictureWrapper>
+                <RedCircle>
+                  <p>X</p>
+                </RedCircle>
+              </WrongAnswerPictureWrapper>
+              <SoundPictureWrapper>
+                <img src={CorrectAnswer} alt="correct_answer" />
+              </SoundPictureWrapper>
+              <SoundPictureWrapper>
+                <img src={Audio} alt="play_button" />
+              </SoundPictureWrapper>
+            </PrimaryPicture>
+ */}
+            <Picture>
+              <img src={TestAnswerPicture} alt="word_picture" />
+              <WordWrapper>
+                <PlayAudioInAnswerCard>
+                  <img src={Audio} alt="play_button" />
+                </PlayAudioInAnswerCard>
+                <Word>
+                  <p className='word'>Academic</p>
+                  <p className='translate'>Перевод</p>
+                </Word>
+              </WordWrapper>
+            </Picture>
+           </PictureBlock>
+        </InnerBlock>
+      </MainBlock>
+    </AudioChallengeWrapper>
   );
 };
 
