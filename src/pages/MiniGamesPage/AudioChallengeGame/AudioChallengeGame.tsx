24ca013821fd85f10f4c4116b9c617d0b119310f
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2022-08-24 19:52:47 +0300

    feat: add green border to input

--- src/pages/MiniGamesPage/AudioChallengeGame/AudioChallengeGame.tsx
+++ src/pages/MiniGamesPage/AudioChallengeGame/AudioChallengeGame.tsx
@@ -3,7 +3,23 @@ import ProgressBar from '../../../components/ProgressBar/ProgressBar';
 import Audio from '../../../assets/AudioBlack.svg';
 import CorrectAnswer from '../../../assets/CorrectAnswer.svg';
 import TestAnswerPicture from '../../../assets/test_answer.jpg';
-import { AnswerButton, AudioChallengeWrapper, ButtonBlock, InnerBlock, MainBlock, Picture, PictureBlock, PlayAudioInAnswerCard, PrimaryPicture, RedCircle, SkipQuestion, SoundPictureWrapper, Word, WordWrapper, WrongAnswerPictureWrapper } from './AudioChallengeGame.styled';
+import {
+  AnswerButton,
+  AudioChallengeWrapper,
+  ButtonBlock,
+  InnerBlock,
+  MainBlock,
+  Picture,
+  PictureBlock,
+  PlayAudioInAnswerCard,
+  PrimaryPicture,
+  RedCircle,
+  SkipQuestion,
+  SoundPictureWrapper,
+  Word,
+  WordWrapper,
+  WrongAnswerPictureWrapper,
+} from './AudioChallengeGame.styled';
 
 const AudioChallengeGame = () => {
   return (
@@ -12,15 +28,14 @@ const AudioChallengeGame = () => {
         <ProgressBar percent={10} />
         <InnerBlock>
           <ButtonBlock>
-            <AnswerButton className='current'>Variant 1</AnswerButton>
-            <AnswerButton className='correct'>Variant</AnswerButton>
-            <AnswerButton className='wrong'>Variant</AnswerButton>
-            <AnswerButton>Variant</AnswerButton>
+            <AnswerButton className="current">Current variant</AnswerButton>
+            <AnswerButton className="correct">Correct variant</AnswerButton>
+            <AnswerButton className="wrong">Wrong variant</AnswerButton>
             <AnswerButton>Variant</AnswerButton>
             <SkipQuestion>Don`t know</SkipQuestion>
           </ButtonBlock>
           <PictureBlock>
-{/*             <PrimaryPicture>
+            {/*             <PrimaryPicture>
               <WrongAnswerPictureWrapper>
                 <RedCircle>
                   <p>X</p>
@@ -41,12 +56,12 @@ const AudioChallengeGame = () => {
                   <img src={Audio} alt="play_button" />
                 </PlayAudioInAnswerCard>
                 <Word>
-                  <p className='word'>Academic</p>
-                  <p className='translate'>Перевод</p>
+                  <p className="word">Academic</p>
+                  <p className="translate">Перевод</p>
                 </Word>
               </WordWrapper>
             </Picture>
-           </PictureBlock>
+          </PictureBlock>
         </InnerBlock>
       </MainBlock>
     </AudioChallengeWrapper>
